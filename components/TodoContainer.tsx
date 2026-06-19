b1638b4c3583747cf2cd5ec1ed21dc391524b155
fatali-fataliyev <fatali.fataliiyev@gmail.com>
2026-06-19 10:58:41 +0400

    applied naming convetion to AddTodoBtn component

--- components/TodoContainer.tsx
+++ components/TodoContainer.tsx
@@ -10,7 +10,7 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
-import AddTodo from "./AddTodoBtn";
+import AddTodo from "./AddTodoButton";
 import EditTodoModal from "./EditTodoModal";
 import TodoItem from "./TodoItem";
 import TodoSearchBar from "./TodoSearchBar";
