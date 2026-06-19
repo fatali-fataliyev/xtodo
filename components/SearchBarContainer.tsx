570490cd7ae0ef32905ac5648e10bdf5c95a3010
fatali-fataliyev <fatali.fataliiyev@gmail.com>
2026-06-19 10:51:22 +0400

    removed unused components

--- components/SearchBarContainer.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export default function SearchBarContainer() {
-  return (
-    <View>
-      <Text style={{ color: "#FFF" }}>Seach bar container</Text>
-    </View>
-  );
-}
