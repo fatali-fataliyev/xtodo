b14453779758c22f96455afd9ddf5acb2cf0bd1c
fatali-fataliyev <fatali.fataliiyev@gmail.com>
2026-06-22 04:57:44 +0400

    implemented first time launch and AES key store logic

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,52 @@
 // app/index.tsx
 
+import { getAESKey, saveAESKey } from "@/utils/secureStorage";
+import * as SecureStore from "expo-secure-store";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect, useState } from "react";
+import { BackHandler } from "react-native";
 import TasksScreen from "./tasks";
 
+SplashScreen.preventAutoHideAsync();
+
 export default function Index() {
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    async function initializeApp() {
+      try {
+        const hasLaunchedBefore =
+          await SecureStore.getItemAsync("is_first_launch");
+
+        if (hasLaunchedBefore !== null) {
+          console.log("NOT FIRST LAUNCH!");
+          const key = await getAESKey();
+
+          if (!key) {
+            BackHandler.exitApp();
+            console.warn(
+              "App initialized but key validation failed/cancelled.",
+            );
+          }
+        } else {
+          console.warn("First launch...");
+          await SecureStore.setItemAsync("is_first_launch", "false");
+          await saveAESKey();
+        }
+      } catch (error) {
+        console.error("Initialization error:", error);
+      } finally {
+        setIsReady(true);
+        await SplashScreen.hideAsync();
+      }
+    }
+
+    initializeApp();
+  }, []);
+
+  if (!isReady) {
+    return null;
+  }
+
   return <TasksScreen />;
 }
