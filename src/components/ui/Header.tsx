a3a834751a1f8bd6d1377c875106bf91c1f6ec53
fatali-fataliyev <fatali.fataliiyev@gmail.com>
2026-08-05 21:43:09 +0400

    minor fixes

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -7,17 +7,5 @@ export default function Header() {
 const styles = StyleSheet.create({
   container: {
     height: 5,
-    justifyContent: "space-between",
-    alignItems: "center",
-    flexDirection: "row",
-  },
-  label: {
-    fontFamily: "Inter-Black",
-    fontSize: 19,
-    marginLeft: 14,
-    color: "#ffffff",
-  },
-  settingsBtn: {
-    marginRight: 14,
   },
 });
